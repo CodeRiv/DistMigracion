ab114ee4f7cd0481d5c0d6d0639369adcb03baa2
Brianrivera1604 <rivera.brian99@gmail.com>
2020-05-25 14:08:09 -0600

    adding cors support

--- src/pages/api/people/[DPI].ts
+++ src/pages/api/people/[DPI].ts
@@ -1,10 +1,30 @@
 import 'module-alias/register';
 import dbConnect from '@utils/dbConnect';
 import People from '@models/People';
+import Cors from 'cors';
+
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+  })
+
+  function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+  
+        return resolve(result)
+      })
+    })
+  }
 
 dbConnect();
 
 export default async (req, res) => {
+
+    await runMiddleware(req, res, cors)
+
     const {
         query: { id, DPI },
         method

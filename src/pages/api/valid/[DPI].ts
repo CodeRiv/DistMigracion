a2a855ac671a771df9d3832021717f3d11f7c101
Brianrivera1604 <rivera.brian99@gmail.com>
2020-05-28 19:38:43 -0600

    Fixing details

--- src/pages/api/valid/[DPI].ts
+++ src/pages/api/valid/[DPI].ts
@@ -48,12 +48,11 @@ export default async (req, res) => {
 
                 else
                 {
-                    
                     res.status(200).json({DPI: DPI, status: "expired"})
 
                 }
             } catch (error) {
-                res.status(400).json({ success: false, data: error});
+                res.status(200).json({ DPI: DPI, status: "Not Valid"});
             }
             break;
     }
